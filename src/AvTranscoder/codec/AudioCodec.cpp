cfe62d8cd676e6634b612158e07bfd655e2543f8
Clement Champetier <cnt@mikrosimage.eu>
2015-01-27 16:47:39 +0100

    hotfix: set fps to 25 for AudioGenerator

--- src/AvTranscoder/codec/AudioCodec.cpp
+++ src/AvTranscoder/codec/AudioCodec.cpp
@@ -24,9 +24,6 @@ AudioFrameDesc AudioCodec::getAudioFrameDesc() const
 {
 	assert( _avCodecContext != NULL );
 	AudioFrameDesc audioFrameDesc( _avCodecContext->sample_rate, _avCodecContext->channels, _avCodecContext->sample_fmt );
-	double fps = 1.0 * _avCodecContext->time_base.den / ( _avCodecContext->time_base.num * _avCodecContext->ticks_per_frame );
-	if( ! isinf( fps ) )
-		audioFrameDesc.setFps( fps );
 	return audioFrameDesc;
 }
 
@@ -35,9 +32,6 @@ void AudioCodec::setAudioParameters( const AudioFrameDesc& audioFrameDesc )
 	_avCodecContext->sample_rate = audioFrameDesc.getSampleRate();
 	_avCodecContext->channels = audioFrameDesc.getChannels();
 	_avCodecContext->sample_fmt = audioFrameDesc.getSampleFormat();
-	_avCodecContext->time_base.num = 1;
-	_avCodecContext->time_base.den = audioFrameDesc.getFps();
-	_avCodecContext->ticks_per_frame = 1;
 }
 
 }
